adce64f06587417520c2234e17483bc3f128f42a
slp-baby <slp-baby@hotmail.com>
2014-12-31 17:29:50 +0800

    usart: modify usart function to use new usart register definitions

--- platform/stm32-common/usart.c
+++ platform/stm32-common/usart.c
@@ -8,23 +8,6 @@
 
 #include INC_PLAT(usart.c)
 
-struct usart_regs {
-	volatile uint16_t SR;
-	uint16_t reserved0;
-	volatile uint16_t DR;
-	uint16_t reserved1;
-	volatile uint16_t BRR;
-	uint16_t reserved2;
-	volatile uint16_t CR1;
-	uint16_t reserved3;
-	volatile uint16_t CR2;
-	uint16_t reserved4;
-	volatile uint16_t CR3;
-	uint16_t reserved5;
-	volatile uint16_t GTPR;
-	uint16_t reserved6;
-};
-
 /* Calculates the value for the USART_BRR */
 /* TODO: Need more precise algorithm */
 static int16_t usart_baud(uint32_t base, uint32_t baud)
@@ -79,50 +62,36 @@ int usart_interrupt_status(struct usart_dev *usart, uint16_t it)
 
 int usart_status(struct usart_dev *usart, uint16_t st)
 {
-	struct usart_regs *uregs;
-
-	uregs = (struct usart_regs *) usart->base;
-
-	return (uregs->SR & st);
+	return (*USART_SR(usart->base) & st);
 }
 
 uint8_t usart_getc(struct usart_dev *usart)
 {
-	struct usart_regs *uregs;
-
-	uregs = (struct usart_regs *) usart->base;
-	return (uregs->DR & 0xff);
+	return (*USART_DR(usart->base) & 0xff);
 }
 
 void usart_putc(struct usart_dev *usart, uint8_t c)
 {
-	struct usart_regs *uregs;
-
-	uregs = (struct usart_regs *) usart->base;
-	uregs->DR = c;
+	*USART_DR(usart->base) = c;
 }
 
 void usart_init(struct usart_dev *usart)
 {
-	struct usart_regs *uregs;
-
 	*(usart->rcc_apbenr) |= usart->rcc_reset;
 	gpio_config(&usart->tx);
 	gpio_config(&usart->rx);
 
-	uregs = (struct usart_regs *) usart->base;
-
-	uregs->CR1 |= USART_CR1_UE;
+	*USART_CR1(usart->base) |= USART_CR1_UE;
 
 	/* FIXME: Hardcode 8-bit */
-	uregs->CR1 &= ~(USART_CR1_M9);
+	*USART_CR1(usart->base) &= ~(USART_CR1_M9);
 
 	/* FIXME: Hardcode 1 stop bit */
-	uregs->CR2 &= ~(3 << 12);
+	*USART_CR2(usart->base) &= ~(3 << 12);
 
 	/* Set baud rate */
-	uregs->BRR = usart_baud(usart->base, usart->baud);
+	*USART_BRR(usart->base) = usart_baud(usart->base, usart->baud);
 
 	/* Enable reciever and transmitter */
-	uregs->CR1 |= (USART_CR1_RE | USART_CR1_TE);
+	*USART_CR1(usart->base) |= (USART_CR1_RE | USART_CR1_TE);
 }
