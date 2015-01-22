5bc4dc2f4a1965d036fe229e2e27a09e82a99566
slp-baby <slp-baby@hotmail.com>
2015-01-22 17:51:29 +0800

    posix: add sem_trywait and sem_wait

--- user/lib/libposix/semaphore.c
+++ user/lib/libposix/semaphore.c
@@ -23,12 +23,38 @@ __USER_TEXT int sem_init(sem_t *sem, int pshared, unsigned int value)
 
 __USER_TEXT int sem_trywait(sem_t *sem)
 {
+    register int result = 1;
 
+    if (sem == NULL)
+        return -1;
+
+    __asm__ __volatile__(
+        "mov r1, #1\n"
+        "mov r2, %[value]\n"
+        "ldrex r0, [r2]\n"
+        "cmp r0, #0\n"
+        "itee eq\n"
+        "moveq %[result], #1\n"
+        "subne r0, r1, r0\n"
+        "movne %[result], #0\n"
+        "mov r1, #0\n"
+        "strex r0, r1, [r2]\n"
+        : [result] "=r"(result)
+        : [value] "r"(&sem->value)
+        : "r0", "r1", "r2");
+
+    return result;
 }
 
 __USER_TEXT int sem_wait(sem_t *sem)
 {
+    if (sem == NULL)
+        return -1;
 
+    while (sem_trywait(sem))
+        L4_Sleep(L4_TimePeriod(1000 * 100));
+
+    return 0;
 }
 
 __USER_TEXT int sem_post(sem_t *sem)
