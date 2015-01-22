496aa2de352f3ab881b6eb782b7fccf25e961696
slp-baby <slp-baby@hotmail.com>
2015-01-22 17:47:20 +0800

    posix: add semaphore library and definition

---
+++ user/lib/libposix/semaphore.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2015 The F9 Microkernel Project. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <l4/ipc.h>
+#include <l4/utcb.h>
+#include <l4/pager.h>
+#include <l4io.h>
+#include <platform/link.h>
+#include <libposix/semaphore.h>
